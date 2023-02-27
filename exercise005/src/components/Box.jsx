c63d24468dac181f1ce87b04cf3bb5c53eaf19f8
Xavha <xavhaswha@gmail.com>
2023-02-27 14:18:39 +0700

    exercise05 done

--- exercise005/src/components/Box.jsx
+++ exercise005/src/components/Box.jsx
@@ -25,7 +25,7 @@ const Box = ({ topic, name, content, img }) => {
             <div className='flex justify-center gap-x-16 items-center'>
                 <div className='basis-1/2'>
                     <h2 className='text-2xl font-bold'>{topic}</h2>
-                    <h3 className='text-lg font-bold'>{name}</h3>
+                    <h3 className='text-lg font-bold my-3'>{name}</h3>
                     <p>{content}</p>
                 </div>
                 <div className='basis-1/4'>
@@ -35,7 +35,7 @@ const Box = ({ topic, name, content, img }) => {
             <div className='inline-flex items-center justify-center gap-x-6'>
                 <Btn name={"Click to Vote"} onClick={Vote} />
                 <h1 className='bg-[#76E300] text-[#8B72C5] text-xl font-bold border-[3px] border-[#8B72C5] rounded-lg p-2'>
-                    {count === 0 ? "Min" : count === 10 ? "Max" : count}
+                    {count === 0 ? "MIN" : count === 10 ? "MAX" : count}
                 </h1>
                 <Btn name={"Click to Unvote"} onClick={Unvote} />
             </div>
--- exercise005/src/components/Button.jsx
+++ exercise005/src/components/Button.jsx
@@ -3,7 +3,8 @@ import React from "react";
 const Btn = ({ name, onClick }) => {
     return (
         <div>
-            <button className="bg-gray-100 hover:bg-gray-200 text-gray-800 py-1 px-4 border border-gray-400 rounded" onClick={onClick} >{name}</button>
+            <button className="bg-gray-100 hover:bg-gray-200 text-gray-800 py-1 px-4 border border-gray-400 rounded"
+                onClick={onClick} >{name}</button>
         </div>
     );
 }
